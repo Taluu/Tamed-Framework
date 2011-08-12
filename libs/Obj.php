9e588f4dfc3a9dc9b63203fae416308088d36bb9
Baptiste Clavie <clavie.b@gmail.com>
2011-08-12 12:03:38 +0200

    getting out Obj of the __init file

---
+++ libs/Obj.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright ©Talus, Talus' Works 2010+
+ * @link http://www.talus-works.net Talus' Works
+ * @license http://www.opensource.org/licenses/BSD-3-Clause Modified BSD License
+ * @version $Id$
+ */
+
+if (!defined('SAFE')) exit;
+
+/**
+ * Global Object Handler
+ *
+ * Contains all the global objects needed by the framework.
+ *
+ * @package twk
+ * @author Baptiste "Talus" Clavié <clavie.b@gmail.com>
+ */
+abstract class Obj {
+  /**
+   * @var \Configuration\Loader
+   */
+  static public $config = null;
+
+  /**
+   * @var \Controller\Front
+   */
+  static public $controller = null;
+
+  /**
+   * @var \Routing\Router
+   */
+  static public $router = null;
+}
